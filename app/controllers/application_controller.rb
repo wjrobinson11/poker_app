a766bededd51ed354822d9cf9717ddb3ef29ebd5
Wesley Robinson <wes@socialcompass.net>
2018-06-29 17:40:16 -0500

    Remove respond to json from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,4 @@ class ApplicationController < ActionController::Base
   acts_as_token_authentication_handler_for User
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
-  respond_to :json
 end
