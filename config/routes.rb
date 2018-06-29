e21ea6401d61fa29e90689d325d511a00f0bf518
Wesley Robinson <wes@socialcompass.net>
2018-06-29 17:42:20 -0500

    Point sessions resource to sessions controller in app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#dashboard'
   get '/dashboard', to: 'pages#dashboard'
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
