3f78c326043c585430fc97787a98e628e9563dfb
Wesley Robinson <wes@socialcompass.net>
2018-06-29 13:17:06 -0500

    Skip before authenticity token for creating users

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  skip_before_action :verify_authenticity_token
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
